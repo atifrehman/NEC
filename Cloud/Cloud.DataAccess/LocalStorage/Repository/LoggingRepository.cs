5c6b26ef4c03984b96853f089fc5156cad398ff7
Atif Rehman <atif_r@outlook.com>
2019-03-24 04:40:57 +0900

    Update LoggingRepository.cs
    Get Method Name and Line Number for Logging implemented

--- Cloud/Cloud.DataAccess/LocalStorage/Repository/LoggingRepository.cs
+++ Cloud/Cloud.DataAccess/LocalStorage/Repository/LoggingRepository.cs
@@ -5,11 +5,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 
 namespace Cloud.DataAccess.LocalStorage.Repository
 {
     public class LoggingRepository : GenericRepository<CloudEntities, Logging>, ILoggingRepository
     {
-        
+        private static int GetLineNumber(Exception ex)
+        {
+            var lineNumber = 0;
+            const string lineSearch = ":line ";
+            var index = ex.StackTrace.LastIndexOf(lineSearch);
+            if (index != -1)
+            {
+                var lineNumberText = ex.StackTrace.Substring(index + lineSearch.Length);
+                if (int.TryParse(lineNumberText, out lineNumber))
+                {
+                }
+            }
+            return lineNumber;
+        }
+
+        private static string GetMethodName(Exception ex)
+        {
+            StackTrace stackTrace = new StackTrace(ex);
+            Assembly thisasm = Assembly.GetExecutingAssembly();
+            string methodname = stackTrace.GetFrames().Select(f => f.GetMethod()).First(m => m.Module.Assembly == thisasm).Name;
+
+            return methodname;
+        }
     }
 }
